2782fdad201aad2e4048106379110dac67047aa5
chancethegreat <pinbal52@gmail.com>
2024-03-04 19:18:56 -0500

    Added link type (incomplete)

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ const form = document.getElementById('generate-form');
 const qr = document.getElementById('qrcode');
 const qrc = document.getElementsByName('qrcodecanvas');
 
+
+
 function onGenerateSubmit(e) {
     e.preventDefault();
     clearUI();
 
-    const url = document.getElementById('url').value;
+    const url = createUrl();
     const size =document.getElementById('size').value;
     const color = document.getElementById('color').value;
 
@@ -23,7 +25,8 @@ function onGenerateSubmit(e) {
 
         setTimeout(function linkURL() {
             const saveUrl = qr.querySelector('img').src;
-            createSaveBtn(saveUrl);
+            const type = document.getElementById('linkType').selectedOptions[0].textContent;
+            createSaveBtn(saveUrl,type);
         },50);
         
         addLogo();
@@ -31,11 +34,60 @@ function onGenerateSubmit(e) {
     }   
 };
 
+/* change placeholder text based on option chosen for link */
+function changeType(option) {
+    
+    var option = document.getElementById('linkType').value;
+    switch (option) {
+        case "01":
+            document.getElementById('type').placeholder='Enter a URL';
+            break;
+        case "02":
+            document.getElementById('type').placeholder='Enter an Email Address';
+            break;
+        case "03":
+            document.getElementById('type').placeholder='Enter a text capable phone number';
+            break;
+        case "04":
+            document.getElementById('type').placeholder='Enter Coordinates. eg. 40.71872,-73.98905,100 ';
+            break;
+        case "05":
+            document.getElementById('type').placeholder='Enter Network Name (SSID)';
+            break;
+        default:
+            document.getElementById('type').placholder='Enter a URL';
+    }
+}
+
+function createUrl() {
+    var a = document.getElementById('type').value;
+    var option = document.getElementById('linkType').value;
+    var url = "";
+
+    switch (option) {
+        case "02":
+            url = "mailto:" + a;
+            break;
+        case "03":
+            url = "sms:" + a;
+            break;
+        case "04":
+            url = "geo:" + a;
+            break;
+        case "05":
+            url = "WIFI:T:WPA;S:" + a + ";P:mypass;;";
+            break;
+        default:
+            url = a;
+    }
+    console.log(url);
+    return url;
+}
+
 function clearUI() {
     document.getElementById('qrcode').innerHTML = "";
     const savelink = document.getElementById('save-link');
     if (savelink) savelink.remove();
-
 };
 
 function generateQRCode(url, size, color) {
@@ -57,13 +109,13 @@ function addLogo() {
 
 };
     
-function createSaveBtn (saveURL) {
+function createSaveBtn (saveURL,type) {
     const link = document.createElement('a');
     link.id = "save-link";
     link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
     link.href = saveURL;
-    link.download = 'qrcode'
-    link.innerHTML = 'Save Image'
+    link.download = type + ' qrcode';
+    link.innerHTML = 'Save Image';
     document.getElementById('generated').appendChild(link);
 };
 
